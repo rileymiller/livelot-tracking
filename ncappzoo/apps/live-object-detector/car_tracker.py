bef4636d4f3a449b4d0d5241fece87af389b4903
Connor Baker <bakerjc49@gmail.com>
2020-01-05 15:04:15 -0700

    proof of concept using the line tracking method

--- ncappzoo/apps/live-object-detector/car_tracker.py
+++ ncappzoo/apps/live-object-detector/car_tracker.py
@@ -96,33 +96,112 @@ class CarTracker:
         self._num_of_frames_to_track = 25
         self._num_cars_in = 0
         self._num_cars_out = 0
+        pointFile = open("points.txt", "r")
+        self._x1 = int(pointFile.readline())
+        self._y1 = int(pointFile.readline())
+        self._x2 = int(pointFile.readline())
+        self._y2 = int(pointFile.readline())
+        self._m = (self._y2 - self._y1) / (self._x1 - self._x2)
+        pointFile.close()
+        self._memory_buffer = []
+        angle = abs(math.degrees(math.atan(self._m)))
+        self._lineVertical = True
+        self._dist_thresh = 10000
+        if angle < 45:
+            self._lineVertical = False
+
+    # -1 = outside 1 = inside
+    def test_point(self, x, y):
+        #print("COORDS", x, y)
+        #print(((y - self._y1 + self._m * self._x1) / self._m))
+        if not self._lineVertical:
+            if x <= max(self._x2, self._x1) and x >= min (self._x1, self._x2):
+                if y > (self._m * (x - self._x1) + self._y1):
+                    #print("below horiz")
+                    return 1
+                else:
+                    #print("above horiz")
+                    return -1
+            else:
+                print("not in col horiz")
+        else:
+            if y <= max(self._y2, self._y1) and y >= min(self._y1, self._y2):
+                if x > ((y - self._y1 + self._m * self._x1) / self._m):
+                    #print("right vert")
+                    return 1
+                else:
+                    #print("left vert")
+                    return -1
+            else:
+                print("not in col vert")
+    
+    def is_obj_in_col(self, obj):
+        x, y = calc_center(obj)
+        if not self._lineVertical:
+            if x <= max(self._x2, self._x1) and x >= min (self._x1, self._x2):
+                return True
+            else:
+                return False
+        else:
+            if y <= max(self._y2, self._y1) and y >= min(self._y1, self._y2):
+                return True
+            else:
+                return False
+    
+    def update_buffer(self, obj):
+        closest_obj, dist, index = self.find_object_in_frame(obj, self._memory_buffer)
+        #will need to figure out good threshold value for cars
+        if dist <= self._dist_thresh:
+            center = calc_center(obj)
+            new_pos_val = self.test_point(center[1], center[0])
+            old_pos_val = self._memory_buffer[index][1]
+            #print("NEW", new_pos_val, "OLD", old_pos_val)
+            if new_pos_val == old_pos_val:
+                print("NO CHANGE")
+            elif new_pos_val == 1 and old_pos_val == -1:
+                print("CAR ENTERED")
+            else:
+                print("CAR EXITED")
+            self._memory_buffer[index][0] = obj
+            self._memory_buffer[index][1] = new_pos_val
+            self._memory_buffer[index][2] = 1
 
     def process_frame(self, frame_number, output_array, output_count):
-        print('I AM IN PROCESS FRAME')
-        print("FOR FRAME ", frame_number)
-        print("Output for each object : ", output_array)
-        print("Output count for unique objects : ", output_count)
-        print("------------END OF A FRAME --------------")
+        #print('I AM IN PROCESS FRAME')
+        #print("FOR FRAME ", frame_number)
+        #print("Output for each object : ", output_array)
+        #print("Output count for unique objects : ", output_count)
+        #print("------------END OF A FRAME --------------")
+
 
         # Get the location of every object in this frame
         this_frame_objects = []
         for i in range(0, output_count):
             obj = output_array.get('detection_boxes_{}'.format(i))
-            this_frame_objects.append(obj)
-        self._tracked_frames.append(this_frame_objects)
-        print(len(self._tracked_frames))
-        if len(self._tracked_frames) == self._num_of_frames_to_track:
-            self.identify_objects()
-            self._tracked_frames = []
+            if self.is_obj_in_col(obj):
+                if not self._memory_buffer:
+                    center = calc_center(obj)
+                    pos_val = self.test_point(center[1], center[0])
+                    temp_arr = [obj, pos_val, 1]
+                    self._memory_buffer.append(temp_arr)
+                else:
+                    #print("UPDATE BUFFER")
+                    self.update_buffer(obj)
+        for i in range(len(self._memory_buffer) - 1, 0, -1):
+            print(i, self._memory_buffer)
+            if self._memory_buffer[i][2] == 0:
+                self._memory_buffer.remove(self._memory_buffer[i])
+        for i in range(0, len(self._memory_buffer) - 1):
+            self._memory_buffer[i][2] = 0
 
     def find_object_in_frame(self, obj1, objs_in_frame):
         num_objs_in_frame = len(objs_in_frame)
         obj1_center = calc_center(obj1)
         closest_obj = None
         closest_obj_dist = 10000
-
+        index = -1
         for i in range(num_objs_in_frame):
-            obj2 = objs_in_frame[i]
+            obj2 = objs_in_frame[i][0]
             obj2_center = calc_center(obj2)
             dist = math.hypot(
                 obj2_center[0] - obj1_center[0], obj2_center[1] - obj1_center[1]
@@ -132,8 +211,9 @@ class CarTracker:
             if dist < closest_obj_dist:
                 closest_obj = obj2
                 closest_obj_dist = dist
+                index = i
 
-        return closest_obj
+        return closest_obj, closest_obj_dist, index
 
     def identify_objects(self):
         frame0 = self._tracked_frames.pop()

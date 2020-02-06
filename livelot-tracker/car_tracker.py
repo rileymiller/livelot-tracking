988ea7cc89d9cf2a9fae263e4c3467099fae0436
Connor Baker <bakerjc49@gmail.com>
2020-02-05 18:13:09 -0700

    merge conflict tag removed

--- livelot-tracker/car_tracker.py
+++ livelot-tracker/car_tracker.py
@@ -10,7 +10,6 @@ def getNumCars(lotname):
     return
 
 def updateCars(val, lotId):
-<<<<<<< HEAD
     try:
         if val == 1:
             response = requests.put('https://livelotapi.herokuapp.com/lot/{}/carIn'.format(lotId))

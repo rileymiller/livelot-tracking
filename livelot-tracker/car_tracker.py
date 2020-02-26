96f18b77c36aa95732e5204f9615d5ac336d1fb0
Connor Baker <bakerjc49@gmail.com>
2020-02-25 21:27:25 -0700

    now uses timeout before making api call as well as minimizes distances between old frame cars and new frame cars to try and deal with stationary cars.

--- livelot-tracker/car_tracker.py
+++ livelot-tracker/car_tracker.py
@@ -4,7 +4,6 @@ logger = logging.getLogger('livelot-tracker.car_tracker')
 import configparser
 import math
 import requests
-from pprint import pprint
 import json
 
 import _thread
@@ -51,7 +50,6 @@ class CarTracker:
         except Exception as e:
             self.logger.error(str(e))
         self._tracked_frames = []
-        self._num_of_frames_to_track = 25
         self._num_cars_in = 0
         self._num_cars_out = 0
         try:
@@ -64,6 +62,7 @@ class CarTracker:
             self.logger.error(str(e))
         self._m = (self._y2 - self._y1) / (self._x1 - self._x2)
         pointFile.close()
+        # 0 - box points, 1 - original position, 2 - new position, 3 - frame update check, 4 - timeout
         self._memory_buffer = []
         angle = abs(math.degrees(math.atan(self._m)))
         self._lineVertical = True
@@ -107,44 +106,68 @@ class CarTracker:
             else:
                 return False
     
-    def update_buffer(self, obj):
-        closest_obj, dist, index = self.find_object_in_frame(obj, self._memory_buffer)
+    def update_buffer(self, this_frame_cars):
+        #closest_obj, dist, index = self.find_object_in_frame(obj, self._memory_buffer)
+        #0 - distance 1 - index of frame obj 2 - index of mem buff
+        min_dist = [9999, -1, -1]
+        #n^2 (n^3 considering loop that calls it) but hopefully its not that many objects to cause performance issues?
+        for new_pos in this_frame_cars:
+            for obj in self._memory_buffer:
+                #already updated continue
+                if obj[3] == 1:
+                    continue
+                prev_pos = obj[0]
+                new_pos_center = calc_center(new_pos)
+                prev_pos_center = calc_center(prev_pos)
+                dist = math.hypot(new_pos_center[0] - prev_pos_center[0], new_pos_center[1] - prev_pos_center[1])
+                if dist < min_dist[0]:
+                    min_dist = [dist, this_frame_cars.index(new_pos), self._memory_buffer.index(obj)]
+        #here
+        obj = this_frame_cars[min_dist[1]]
+        index = min_dist[2]
         #will need to figure out good threshold value for cars
-        if dist <= self._dist_thresh:
-            center = calc_center(obj)
-            new_pos_val = self.test_point(center[0], center[1])
-            old_pos_val = self._memory_buffer[index][1]
-            self._memory_buffer[index][0] = obj
-            self._memory_buffer[index][1] = new_pos_val
-            self._memory_buffer[index][2] = 1
-            if new_pos_val == old_pos_val:
-                return
-            elif new_pos_val == 1 and old_pos_val == -1:
-
-                _thread.start_new_thread(updateCars,(1, self._lotId))
-            else:
-                _thread.start_new_thread(updateCars,(-1, self._lotId))
-
+        center = calc_center(obj)
+        new_pos_val = self.test_point(center[0], center[1])
+        old_pos_val = self._memory_buffer[index][1]
+        self._memory_buffer[index][0] = obj
+        self._memory_buffer[index][2] = new_pos_val
+        self._memory_buffer[index][3] = 1
+        self._memory_buffer[index][4] = 3
+            
+    def remove_from_buffer(self, index):
+        old_pos_val = self._memory_buffer[index][1]
+        new_pos_val = self._memory_buffer[index][2]
+        self._memory_buffer.remove(self._memory_buffer[index])
+        if new_pos_val == old_pos_val or new_pos_val == 0:
+            return
+        elif new_pos_val == 1 and old_pos_val == -1:
+            _thread.start_new_thread(updateCars,(1, self._lotId))
+        else:
+            _thread.start_new_thread(updateCars,(-1, self._lotId))
 
     def process_frame(self, output_array):
         # Get the location of every object in this frame
-        this_frame_objects =[]
+        this_frame_cars =[]
+        for obj in output_array:
+            this_frame_cars.append(obj[0])
+                   
         for obj in output_array:
             obj = obj[0]
-            if self.is_obj_in_col(obj):
-                closest_obj, dist, index = self.find_object_in_frame(obj, self._memory_buffer)
-                if closest_obj == None:
-                    center = calc_center(obj)
-                    pos_val = self.test_point(center[0], center[1])
-                    temp_arr = [obj, pos_val, 1]
-                    self._memory_buffer.append(temp_arr)
-                else:
-                    self.update_buffer(obj)
+            closest_obj, dist, index = self.find_object_in_frame(obj, self._memory_buffer)
+            if closest_obj == None:
+                center = calc_center(obj)
+                pos_val = self.test_point(center[0], center[1])
+                temp_arr = [obj, pos_val, 0,  1, 3]
+                self._memory_buffer.append(temp_arr)
+            else:
+                self.update_buffer(this_frame_cars)
         for i in range(len(self._memory_buffer) - 1, -1, -1):
-            if self._memory_buffer[i][2] == 0:
-                self._memory_buffer.remove(self._memory_buffer[i])
+            if self._memory_buffer[i][3] == 0 and self._memory_buffer[i][4] == 0:
+                self.remove_from_buffer(i)
+            elif self._memory_buffer[i][3] == 0:
+                self._memory_buffer[i][4] = self._memory_buffer[i][4] - 1
         for i in range(0, len(self._memory_buffer)):
-            self._memory_buffer[i][2] = 0
+            self._memory_buffer[i][3] = 0
 
     def find_object_in_frame(self, obj1, objs_in_frame):
         num_objs_in_frame = len(objs_in_frame)
@@ -160,7 +183,7 @@ class CarTracker:
             )
 
             # TODO there should probably be some sort of thresholding done here
-            if dist < closest_obj_dist and objs_in_frame[i][2] != 1:
+            if dist < closest_obj_dist and objs_in_frame[i][3] != 1:
                 closest_obj = obj2
                 closest_obj_dist = dist
                 index = i
--- pisetup.md
+++ pisetup.md
@@ -1,50 +1,19 @@
-## Setting up a Raspberry Pi from scratch
+1. Head to https://www.raspberrypi.org/downloads/ and download the latest version of Rasbian (Raspian Buster with desktop)
+2. Flash the sd card with the image.
+3. Connect the pi to a display and plug in a keyboard and mouse.
+4. Setup the timezone, password, wifi,  and install the latest software.
+5. Clone the `livelot-tracking` repository into the home directory.
+6. `sudo apt-get install dnsutils`
+6. Edit `/etc/rc.local` and add `sleep 30s` and `python /home/pi/livelot-tracking/pifind.py`
+7. Go to the top left dropdown > Preferences > Raspberry Pi Configuration > Interfaces and enable Camera and SSH.
+8. Visit `https://download.01.org/opencv/2020/openvinotoolkit/` and find the latest version of the OpenVino toolkin and download it I.E. `wget https://download.01.org/opencv/2020/openvinotoolkit/2020.1/l_openvino_toolkit_runtime_raspbian_p_2020.1.023.tgz`
+9. sudo mkdir -p /opt/intel/openvino
+10. `sudo tar -xf  l_openvino_toolkit_runtime_raspbian_p_<version>.tgz --strip 1 -C /opt/intel/openvino`
+11. `sudo apt install cmake`
+12. `source /opt/intel/openvino/bin/setupvars.sh`
+13. `echo "source /opt/intel/openvino/bin/setupvars.sh" >> ~/.bashrc`
+14. `sudo usermod -a -G users "$(whoami)"`
+15. reboot the pi
+16. `sh /opt/intel/openvino/install_dependencies/install_NCS_udev_rules.sh`
 
-1. Image the Pi with the most recent RPi image: 
-``` 
-    git clone https://www.raspberrypi.org/downloads/raspberry-pi-desktop/
-```
-2. Enable Raspberry Pi for ssh if not already enabled and enable Pi Camera:
-    - Launch `Raspberry Pi Configuration` from the `Preferences` menu
-    - Navigate to the `Interfaces` tab
-    - Select `Enabled` next to `SSH`
-    - Select `Enabled` next to `Camera`
-    - Click `OK`
 
-3. Place `pifind.py` under `/home/pi/` (TODO write `install.sh` script) and change privileges running:
-```
-    chmod 777 pifind.py
-``` 
-then edit `/etc/rc.local` and add two lines in the file:
-    ```
-        sleep 30s
-        python /home/pi/pifind.py
-    ```
-4. Pull down the `livelot-tracking` repo: 
-```
-    git clone https://github.com/rileymiller/livelot-tracking
-```
-5. Install all Debian and Python dependencies for the RPi and NCS:
-```
-    sudo apt-get install -y libusb-1.0-0-dev libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler libatlas-base-dev git automake byacc lsb-release cmake libgflags-dev libgoogle-glog-dev liblmdb-dev swig3.0 graphviz libxslt-dev libxml2-dev gfortran python3-dev python-pip python3-pip python3-setuptools python3-markdown python3-pillow python3-yaml python3-pygraphviz python3-h5py python3-nose python3-lxml python3-matplotlib python3-numpy python3-protobuf python3-dateutil python3-skimage python3-scipy python3-six python3-networkx python3-tk
-```
-6. Navigate into `livelot-tracking` and compile NCSDK's API framework:
-```
-    cd ~/livelot-tracking/ncsdk
-    make
-    sudo make install
-```
-7. Install OpenCV and additional dependencies for RPi:
-```
-    sudo apt-get install python-opencv
-    sudo pip3 install opencv-python==3.4.3.18
-    sudo apt-get install libjasper-dev
-    sudo apt-get install libqtgui4
-    sudo apt-get install libqt4-test
-    sudo pip3 install boto3
-```
-8. Navigate and run script to detect cars:
-```
-    cd ~/livelot-tracking/ncsappzoo/apps/livelot-object-detector
-    python3 live-object-detector-copy.py (TODO update this with correct naming conventions)
-```

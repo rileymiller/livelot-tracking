07cebb1799ff340e140b46874e76beb35906ab80
Connor Baker <bakerjc49@gmail.com>
2020-03-10 13:05:57 -0600

    Added comments to the pifind script

--- pifind.py
+++ pifind.py
@@ -22,8 +22,10 @@ ipv4 = subprocess.Popen("hostname -I", shell=True, stdout=subprocess.PIPE).commu
 ipv4 = ipv4.decode("utf-8")
 ipv4 = ipv4.split()[0]
 
+#Get the ipv6 address from the linux dig command
 ipv6 = subprocess.Popen("dig TXT +short o-o.myaddr.l.google.com @ns1.google.com", shell=True, stdout=subprocess.PIPE).communicate()[0]
 ipv6 = ipv6.decode("utf-8")
+#The ipv6 address comes back with double qoutes so strip them
 ipv6 = re.findall(r'"([^"]*)"', ipv6)[0]
 
 sio.emit("camera-connection", {"ipv4": ipv4, "ipv6": ipv6, "cameraID": cameraID})
